a188729d02e69a9c1824bb8b89a65ce87735412a
EiEiKyaw <eieikyaw20101968@gmail.com>
2018-06-06 20:23:16 +0630

    Write ex5.py

---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name='Ei Ei Kyaw'
+my_age=22 #not a lie
+my_height=62#inches
+my_weight=115#lbs
+my_eyes='Black'
+my_teeth='White'
+my_hair='Brown'
+
+
+print(f"Let's talk about {my_name}.")
+print(f"She's {my_height} inches tall.")
+print(f"She's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} depending on the coffee.")
+
+#this line is tricky, try to get it exactly right
+total=my_age+my_height+my_weight
+print(f"If I add {my_age}, {my_height},and {my_weight} I get {total}.")
