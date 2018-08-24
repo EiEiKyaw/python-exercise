63dcc073f55f125de784eb61aa7c82a50ca706ed
EiEiKyaw <eieikyaw20101968@gmail.com>
2018-08-24 15:28:25 -0800

    add jinja

--- ex8.py
+++ ex8.py
@@ -4,5 +4,5 @@ print(formatter.format(1,2,3,4))
 print(formatter.format("one","two","three","four"))
 print(formatter.format(True,False,False,True))
 print(formatter.format(formatter,formatter,formatter,formatter))
-print(formatter.format("Tryyour","Own text here","Maybe a poem","Or a song about fear"))
+print(formatter.format("Try our","Own text here","Maybe a poem","Or a song about fear"))
 
---
+++ jinja/main.py
@@ -0,0 +1,37 @@
+
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/unitowin")
+def unitowin():
+    return render_template("unitowin.html")
+
+@app.route("/wintouni")
+def wintouni():
+    return render_template("wintouni.html")
+
+@app.route("/wintozawgyi")
+def wintozawgyi():
+    return render_template("wintozawgyi.html")
+
+@app.route("/zawgyitouni")
+def zawgyitouni():
+    return render_template("zawgyitouni.html")
+
+@app.route("/zawgyitowin")
+def zawgyitowin():
+    return render_template("zawgyitowin.html")
+
+@app.route("/firstpages")
+def firstpage():
+    return render_template("firstpage.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
